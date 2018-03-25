0551286d48d4a60fc056bd5e6df485c4916787a8
Megamind <srivastavapiyush00@gmail.com>
2018-03-25 10:29:24 +0530

    First Time git set up

---
+++ MavenProject/src/main/java/Test/NewTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void firstMethod()
+	{
+	System.out.println("Integration Program");	
+	}
+
+}
---
+++ MavenProject/src/test/java/Demo/NewTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void firstMethod()
+	{
+	System.out.println("Integration Program");	
+	}
+
+}
